cdeabd30b797ca46d1ed03e9bfb6fa590451e5fc
Tyler Teran <teran.tyler@yahoo.com>
2019-03-08 07:15:32 -0500

    first commit

---
+++ CoasterProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoasterProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
