36190ac524491639df0df45e9f076cc537846b85
Tyler Teran <teran.tyler@yahoo.com>
2019-03-11 14:37:54 -0400

    Updated DB with my updates.

---
+++ CoasterProject/Controllers/RideTrackerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoasterProject.Controllers
+{
+    public class RideTrackerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
