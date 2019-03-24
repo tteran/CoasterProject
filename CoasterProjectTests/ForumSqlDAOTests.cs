b387b9c0929c276228cf47b0b85a25fb5d50c8b1
Tyler Teran <teran.tyler@yahoo.com>
2019-03-24 13:10:36 -0400

    Added new test for the ForumPostSqlDAO.

---
+++ CoasterProjectTests/ForumSqlDAOTests.cs
@@ -0,0 +1,21 @@
+﻿using CoasterProject.DAL;
+using CoasterProject.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoasterProjectTests
+{
+    [TestClass]
+    public class ForumSqlDAOTests : CoasterDAOTests
+    {
+       [TestMethod]
+       public void ListOfAllPosts_ShouldReturn_AllPosts()
+        {
+            ForumSqlDAO dao = new ForumSqlDAO(ConnectionString);
+            IList<ForumPost> forumPosts = dao.GetPosts();
+            Assert.AreEqual(1, forumPosts.Count);
+        }
+    }
+}
